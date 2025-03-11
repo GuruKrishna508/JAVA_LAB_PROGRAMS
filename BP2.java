2a148c1db31a5c2ebe1ab3e4e6c564fde6f2156d
GuruKrishna508 <gksharma98458@gmail.com>
2025-03-11 23:25:03 +0530

    2nd program related to getter,setter and tostring concept

---
+++ BP2.java
@@ -0,0 +1,29 @@
+class Book{
+   String name,author,price,pages;
+   void setter(String name,String author,String price,String pages){
+    this.name=name;
+    this.author=author;
+    this.price=price;
+    this.pages=pages;
+   }
+   String getter(){
+    return "name:"+name+" author:"+author+" price:"+price+" pages:"+pages;
+
+   }
+
+}
+public class BP2 {
+    public static void main(String [] args){
+        Book[] book=new Book[2];
+        book[0]=new Book();
+        book[0].setter("Divine comedy","Dante Algeri","160","60");
+        System.out.println(book[0].getter().toString());
+        book[1]=new Book();
+        book[1].setter("Divine comedy","Dante Algeri","160","60");
+        System.out.println(book[1].getter().toString());
+      
+
+
+    }
+    
+}
